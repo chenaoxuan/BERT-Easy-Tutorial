b62ce22bc2aae385da83bb8d05c2f5b7616bc202
chenaoxuan <cax1165@163.com>
2024-07-30 22:31:52 +0800

    LayerNorm Update

--- bert/model/utils/layer_norm.py
+++ bert/model/utils/layer_norm.py
@@ -5,13 +5,13 @@ import torch.nn as nn
 class LayerNorm(nn.Module):
     "Construct a layernorm module (See citation for details)."
 
-    def __init__(self, features, eps=1e-6):
+    def __init__(self, hidden, eps=1e-6):
         super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
+        self._gamma = nn.Parameter(torch.ones(hidden))
+        self._beta = nn.Parameter(torch.zeros(hidden))
         self.eps = eps
 
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+        return self._gamma * (x - mean) / (std + self.eps) + self._beta
